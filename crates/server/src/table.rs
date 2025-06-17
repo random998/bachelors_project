816417d54e7376b3edbcbb0f8165e85630239ca4
random998 <some.email42@keemail.me>
2025-06-17 19:14:51 +0200

    add code from freezeout poker to file table.rs in the server crate

--- crates/server/src/table.rs
+++ crates/server/src/table.rs
@@ -1,6 +1,5 @@
-// code taken from https://github.com/vincev/freezeout
+// Refactored poker table implementation for improved clarity and structure.
 
-//! poker table implementation.
 use anyhow::Result;
 use log::{error, info};
 use std::{sync::Arc, time::Duration};
@@ -15,30 +14,24 @@ use freezeout_core::{
     poker::{Chips, TableId},
 };
 
-use crate::db::Db;
+use crate::db::Database;
 
 mod player;
 mod state;
 
 pub use state::TableJoinError;
 
-/// table state shared between players.
 #[derive(Debug)]
 pub struct Table {
-    commands_tx: mpsc::Sender<TableCommand>, /// channel for sending commands.
+    command_sender: mpsc::Sender<TableCommand>,
     table_id: TableId,
 }
 
-/// message structure to send to players from table.
 #[derive(Debug)]
 pub enum TableMessage {
-    /// sends signed message to client.
     Send(SignedMessage),
-    /// instruct client to leave table.
     PlayerLeave,
-    /// instruct client to introduce delay between messages.
     Throttle(Duration),
-    /// Close a client connection.
     Close,
 }
 
@@ -48,60 +41,137 @@ enum TableCommand {
         nickname: String,
         join_chips: Chips,
         table_tx: mpsc::Sender<TableMessage>,
-        response_tx: mpsc::Sender<Result<(), State:TableJoinError>>,
+        response_tx: mpsc::Sender<Result<(), TableJoinError>>,
     },
-    /// query if a player can join.
-    CanPlayerJoin {response_tx: oneshot::Sender<bool>},
-    /// leave this table.
+    CanPlayerJoin {
+        response_tx: oneshot::Sender<bool>,
+    },
     Leave(PeerId),
-    /// handle a player's signed message.
-    HandleMessage(SignedMessage)
+    HandleMessage(SignedMessage),
 }
 
 impl Table {
     pub fn new(
-          num_seats: usize,
-          signing_key: Arc<SigningKey>,
-          database: Db,
-          shutdown_broadcast_rx: broadcast::Sender<()>,
-          shutdown_complete_tx: mpsc::Sender<()>,
-      ) -> Table {
-          assert!(seats > 1, "at least two seats should be available");
+        seats: usize,
+        signing_key: Arc<SigningKey>,
+        database: Database,
+        shutdown_rx: broadcast::Receiver<()>,
+        shutdown_complete_tx: mpsc::Sender<()>,
+    ) -> Self {
+        assert!(seats > 1, "A table must have at least two seats");
 
-          let (commands_tx, commands_rx) = mpsc::channel::<TableCommand>(128); //TODO: meaning of 128?
-          let table_id = TableId::new_id();
+        let (command_sender, command_receiver) = mpsc::channel::<TableCommand>(128);
+        let table_id = TableId::new_id();
 
-          let mut task = TableTask {
-              table_id,
-              num_seats,
-              signing_key,
-              database,
-              commands_rx,
-              shutdown_broadcast_rx,
-              _shutdown_complete_tx: shutdown_complete_tx,
-          };
+        let mut task = TableTask {
+            table_id,
+            seats,
+            signing_key,
+            database,
+            command_receiver,
+            shutdown_rx,
+            shutdown_complete_tx,
+        };
 
-          tokio::spawn(async move {
-              if let Err(err) = task.run().await {
-                  error!("table {} error: {}", task.table_id, err);
-              }
-              info!("table task for table {} stopped", task.table_id);
-          });
+        tokio::spawn(async move {
+            if let Err(err) = task.run().await {
+                error!("Table {} encountered an error: {}", task.table_id, err);
+            }
+            info!("Table task {} has been stopped", task.table_id);
+        });
 
-          Table {
-              commands_tx,
-              table_id,
-          }
-      }
-      pub fn table_id(&self) -> TableId {
-          self.table_id
-      }
+        Self {
+            command_sender,
+            table_id,
+        }
+    }
 
-      pub async fn can_player_join(&self) -> bool {
-          let (response_tx, response_rx) = oneshot::channel();
-      }
+    pub fn id(&self) -> TableId {
+        self.table_id
+    }
 
+    pub async fn can_player_join(&self) -> bool {
+        let (response_tx, response_rx) = oneshot::channel();
+        let sent = self.command_sender.send(TableCommand::CanPlayerJoin { response_tx }).await.is_ok();
+        sent && response_rx.await.unwrap_or(false)
+    }
+
+    pub async fn try_join(
+        &self,
+        player_id: &PeerId,
+        nickname: &str,
+        chips: Chips,
+        table_tx: mpsc::Sender<TableMessage>,
+    ) -> Result<(), TableJoinError> {
+        let (response_tx, response_rx) = oneshot::channel();
+
+        self.command_sender
+            .send(TableCommand::TryJoin {
+                player_id: player_id.clone(),
+                nickname: nickname.to_string(),
+                join_chips: chips,
+                table_tx,
+                response_tx,
+            })
+            .await
+            .map_err(|_| TableJoinError::Unknown)?;
+
+        response_rx.await.map_err(|_| TableJoinError::Unknown)?
+    }
+
+    pub async fn leave(&self, player_id: &PeerId) {
+        let _ = self.command_sender.send(TableCommand::Leave(player_id.clone())).await;
+    }
+
+    pub async fn handle_message(&self, msg: SignedMessage) {
+        let _ = self.command_sender.send(TableCommand::HandleMessage(msg)).await;
+    }
 }
 
+struct TableTask {
+    table_id: TableId,
+    seats: usize,
+    signing_key: Arc<SigningKey>,
+    database: Database,
+    command_receiver: mpsc::Receiver<TableCommand>,
+    shutdown_rx: broadcast::Receiver<()>,
+    shutdown_complete_tx: mpsc::Sender<()>,
+}
 
+impl TableTask {
+    async fn run(&mut self) -> Result<()> {
+        let mut state = state::State::new(
+            self.table_id,
+            self.seats,
+            self.signing_key.clone(),
+            self.database.clone(),
+        );
+        let mut ticker = time::interval(Duration::from_millis(500));
+
+        loop {
+            tokio::select! {
+                _ = self.shutdown_rx.recv() => break Ok(()),
+                _ = ticker.tick() => state.tick().await,
+                command = self.command_receiver.recv() => {
+                    match command {
+                        Some(TableCommand::TryJoin { player_id, nickname, join_chips, table_tx, response_tx }) => {
+                            let result = state.try_join(&player_id, &nickname, join_chips, table_tx).await;
+                            let _ = response_tx.send(result);
+                        }
+                        Some(TableCommand::CanPlayerJoin { response_tx }) => {
+                            let can_join = state.player_can_join();
+                            let _ = response_tx.send(can_join);
+                        }
+                        Some(TableCommand::Leave(pid)) => {
+                            state.leave(&pid).await;
+                        }
+                        Some(TableCommand::HandleMessage(msg)) => {
+                            state.message(&msg).await;
+                        }
+                        None => break Ok(()),
+                    }
+                }
+            }
+        }
+    }
 }
