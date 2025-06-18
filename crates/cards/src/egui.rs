d5bbc6138adcccd5bc97151bfa5a3f11e36d0f9b
random998 <some.email42@keemail.me>
2025-06-18 16:02:53 +0200

    fix egui.rs card code. TODO: duplicate code from cards.rs

--- crates/cards/src/egui.rs
+++ crates/cards/src/egui.rs
@@ -1,4 +1,5 @@
 // code taken from https://github.com/vincev/freezeout
+//TODO: duplicate code? cards.rs
 
 //! Cards images loading and painting.
 use ahash::AHashMap;
@@ -7,129 +8,128 @@ use std::sync::LazyLock;
 
 use crate::deck::{Card, Deck, Rank, Suit};
 
-/// The clubs.
-const BYTES_2C: &[u8] = include_bytes!("assets/2c.png");
-const BYTES_3C: &[u8] = include_bytes!("assets/3c.png");
-const BYTES_4C: &[u8] = include_bytes!("assets/4c.png");
-const BYTES_5C: &[u8] = include_bytes!("assets/5c.png");
-const BYTES_6C: &[u8] = include_bytes!("assets/6c.png");
-const BYTES_7C: &[u8] = include_bytes!("assets/7c.png");
-const BYTES_8C: &[u8] = include_bytes!("assets/8c.png");
-const BYTES_9C: &[u8] = include_bytes!("assets/9c.png");
-const BYTES_TC: &[u8] = include_bytes!("assets/tc.png");
-const BYTES_JC: &[u8] = include_bytes!("assets/jc.png");
-const BYTES_KC: &[u8] = include_bytes!("assets/kc.png");
-const BYTES_QC: &[u8] = include_bytes!("assets/qc.png");
-const BYTES_AC: &[u8] = include_bytes!("assets/ac.png");
+// clubs.
+const BYTES_2_OF_CLUBS: &[u8] = include_bytes!("assets/2_of_clubs.png");
+const BYTES_3_OF_CLUBS: &[u8] = include_bytes!("assets/3_of_clubs.png");
+const BYTES_4_OF_CLUBS: &[u8] = include_bytes!("assets/4_of_clubs.png");
+const BYTES_5_OF_CLUBS: &[u8] = include_bytes!("assets/5_of_clubs.png");
+const BYTES_6_OF_CLUBS: &[u8] = include_bytes!("assets/6_of_clubs.png");
+const BYTES_7_OF_CLUBS: &[u8] = include_bytes!("assets/7_of_clubs.png");
+const BYTES_8_OF_CLUBS: &[u8] = include_bytes!("assets/8_of_clubs.png");
+const BYTES_9_OF_CLUBS: &[u8] = include_bytes!("assets/9_of_clubs.png");
+const BYTES_10_OF_CLUBS: &[u8] = include_bytes!("assets/10_of_clubs.png");
+const BYTES_JACK_OF_CLUBS: &[u8] = include_bytes!("assets/jack_of_clubs.png");
+const BYTES_QUEEN_OF_CLUBS: &[u8] = include_bytes!("assets/queen_of_clubs.png");
+const BYTES_KING_OF_CLUBS: &[u8] = include_bytes!("assets/king_of_clubs.png");
+const BYTES_ACE_OF_CLUBS: &[u8] = include_bytes!("assets/ace_of_clubs.png");
 
-/// The diamonds.
-const BYTES_2D: &[u8] = include_bytes!("assets/2d.png");
-const BYTES_3D: &[u8] = include_bytes!("assets/3d.png");
-const BYTES_4D: &[u8] = include_bytes!("assets/4d.png");
-const BYTES_5D: &[u8] = include_bytes!("assets/5d.png");
-const BYTES_6D: &[u8] = include_bytes!("assets/6d.png");
-const BYTES_7D: &[u8] = include_bytes!("assets/7d.png");
-const BYTES_8D: &[u8] = include_bytes!("assets/8d.png");
-const BYTES_9D: &[u8] = include_bytes!("assets/9d.png");
-const BYTES_TD: &[u8] = include_bytes!("assets/td.png");
-const BYTES_JD: &[u8] = include_bytes!("assets/jd.png");
-const BYTES_KD: &[u8] = include_bytes!("assets/kd.png");
-const BYTES_QD: &[u8] = include_bytes!("assets/qd.png");
-const BYTES_AD: &[u8] = include_bytes!("assets/ad.png");
+// Diamonds
+const BYTES_2_OF_DIAMONDS: &[u8] = include_bytes!("assets/2_of_diamonds.png");
+const BYTES_3_OF_DIAMONDS: &[u8] = include_bytes!("assets/3_of_diamonds.png");
+const BYTES_4_OF_DIAMONDS: &[u8] = include_bytes!("assets/4_of_diamonds.png");
+const BYTES_5_OF_DIAMONDS: &[u8] = include_bytes!("assets/5_of_diamonds.png");
+const BYTES_6_OF_DIAMONDS: &[u8] = include_bytes!("assets/6_of_diamonds.png");
+const BYTES_7_OF_DIAMONDS: &[u8] = include_bytes!("assets/7_of_diamonds.png");
+const BYTES_8_OF_DIAMONDS: &[u8] = include_bytes!("assets/8_of_diamonds.png");
+const BYTES_9_OF_DIAMONDS: &[u8] = include_bytes!("assets/9_of_diamonds.png");
+const BYTES_10_OF_DIAMONDS: &[u8] = include_bytes!("assets/10_of_diamonds.png");
+const BYTES_JACK_OF_DIAMONDS: &[u8] = include_bytes!("assets/jack_of_diamonds.png");
+const BYTES_QUEEN_OF_DIAMONDS: &[u8] = include_bytes!("assets/queen_of_diamonds.png");
+const BYTES_KING_OF_DIAMONDS: &[u8] = include_bytes!("assets/king_of_diamonds.png");
+const BYTES_ACE_OF_DIAMONDS: &[u8] = include_bytes!("assets/ace_of_diamonds.png");
 
-/// The hearts.
-const BYTES_2H: &[u8] = include_bytes!("assets/2h.png");
-const BYTES_3H: &[u8] = include_bytes!("assets/3h.png");
-const BYTES_4H: &[u8] = include_bytes!("assets/4h.png");
-const BYTES_5H: &[u8] = include_bytes!("assets/5h.png");
-const BYTES_6H: &[u8] = include_bytes!("assets/6h.png");
-const BYTES_7H: &[u8] = include_bytes!("assets/7h.png");
-const BYTES_8H: &[u8] = include_bytes!("assets/8h.png");
-const BYTES_9H: &[u8] = include_bytes!("assets/9h.png");
-const BYTES_TH: &[u8] = include_bytes!("assets/th.png");
-const BYTES_JH: &[u8] = include_bytes!("assets/jh.png");
-const BYTES_KH: &[u8] = include_bytes!("assets/kh.png");
-const BYTES_QH: &[u8] = include_bytes!("assets/qh.png");
-const BYTES_AH: &[u8] = include_bytes!("assets/ah.png");
+// Hearts
+const BYTES_2_OF_HEARTS: &[u8] = include_bytes!("assets/2_of_hearts.png");
+const BYTES_3_OF_HEARTS: &[u8] = include_bytes!("assets/3_of_hearts.png");
+const BYTES_4_OF_HEARTS: &[u8] = include_bytes!("assets/4_of_hearts.png");
+const BYTES_5_OF_HEARTS: &[u8] = include_bytes!("assets/5_of_hearts.png");
+const BYTES_6_OF_HEARTS: &[u8] = include_bytes!("assets/6_of_hearts.png");
+const BYTES_7_OF_HEARTS: &[u8] = include_bytes!("assets/7_of_hearts.png");
+const BYTES_8_OF_HEARTS: &[u8] = include_bytes!("assets/8_of_hearts.png");
+const BYTES_9_OF_HEARTS: &[u8] = include_bytes!("assets/9_of_hearts.png");
+const BYTES_10_OF_HEARTS: &[u8] = include_bytes!("assets/10_of_hearts.png");
+const BYTES_JACK_OF_HEARTS: &[u8] = include_bytes!("assets/jack_of_hearts.png");
+const BYTES_QUEEN_OF_HEARTS: &[u8] = include_bytes!("assets/queen_of_hearts.png");
+const BYTES_KING_OF_HEARTS: &[u8] = include_bytes!("assets/king_of_hearts.png");
+const BYTES_ACE_OF_HEARTS: &[u8] = include_bytes!("assets/ace_of_hearts.png");
 
-/// The spades.
-const BYTES_2S: &[u8] = include_bytes!("assets/2s.png");
-const BYTES_3S: &[u8] = include_bytes!("assets/3s.png");
-const BYTES_4S: &[u8] = include_bytes!("assets/4s.png");
-const BYTES_5S: &[u8] = include_bytes!("assets/5s.png");
-const BYTES_6S: &[u8] = include_bytes!("assets/6s.png");
-const BYTES_7S: &[u8] = include_bytes!("assets/7s.png");
-const BYTES_8S: &[u8] = include_bytes!("assets/8s.png");
-const BYTES_TS: &[u8] = include_bytes!("assets/ts.png");
-const BYTES_9S: &[u8] = include_bytes!("assets/9s.png");
-const BYTES_JS: &[u8] = include_bytes!("assets/js.png");
-const BYTES_KS: &[u8] = include_bytes!("assets/ks.png");
-const BYTES_QS: &[u8] = include_bytes!("assets/qs.png");
-const BYTES_AS: &[u8] = include_bytes!("assets/as.png");
-
+// Spades
+const BYTES_2_OF_SPADES: &[u8] = include_bytes!("assets/2_of_spades.png");
+const BYTES_3_OF_SPADES: &[u8] = include_bytes!("assets/3_of_spades.png");
+const BYTES_4_OF_SPADES: &[u8] = include_bytes!("assets/4_of_spades.png");
+const BYTES_5_OF_SPADES: &[u8] = include_bytes!("assets/5_of_spades.png");
+const BYTES_6_OF_SPADES: &[u8] = include_bytes!("assets/6_of_spades.png");
+const BYTES_7_OF_SPADES: &[u8] = include_bytes!("assets/7_of_spades.png");
+const BYTES_8_OF_SPADES: &[u8] = include_bytes!("assets/8_of_spades.png");
+const BYTES_9_OF_SPADES: &[u8] = include_bytes!("assets/9_of_spades.png");
+const BYTES_10_OF_SPADES: &[u8] = include_bytes!("assets/10_of_spades.png");
+const BYTES_JACK_OF_SPADES: &[u8] = include_bytes!("assets/jack_of_spades.png");
+const BYTES_QUEEN_OF_SPADES: &[u8] = include_bytes!("assets/queen_of_spades.png");
+const BYTES_KING_OF_SPADES: &[u8] = include_bytes!("assets/king_of_spades.png");
+const BYTES_ACE_OF_SPADES: &[u8] = include_bytes!("assets/ace_of_spades.png");
 /// The cards back.
-const BYTES_BB: &[u8] = include_bytes!("assets/bb.png");
+const BYTES_CARD_BACK: &[u8] = include_bytes!("assets/card_back.png");
 
 static CARD_IMAGES: LazyLock<AHashMap<Card, &'static [u8]>> = LazyLock::new(|| {
-    let mut cards = AHashMap::with_capacity(Deck::SIZE);
+        let mut cards = AHashMap::with_capacity(Deck::SIZE);
 
-    cards.insert(Card::new(Rank::Deuce, Suit::Clubs), BYTES_2C);
-    cards.insert(Card::new(Rank::Trey, Suit::Clubs), BYTES_3C);
-    cards.insert(Card::new(Rank::Four, Suit::Clubs), BYTES_4C);
-    cards.insert(Card::new(Rank::Five, Suit::Clubs), BYTES_5C);
-    cards.insert(Card::new(Rank::Six, Suit::Clubs), BYTES_6C);
-    cards.insert(Card::new(Rank::Seven, Suit::Clubs), BYTES_7C);
-    cards.insert(Card::new(Rank::Eight, Suit::Clubs), BYTES_8C);
-    cards.insert(Card::new(Rank::Nine, Suit::Clubs), BYTES_9C);
-    cards.insert(Card::new(Rank::Ten, Suit::Clubs), BYTES_TC);
-    cards.insert(Card::new(Rank::Jack, Suit::Clubs), BYTES_JC);
-    cards.insert(Card::new(Rank::Queen, Suit::Clubs), BYTES_QC);
-    cards.insert(Card::new(Rank::King, Suit::Clubs), BYTES_KC);
-    cards.insert(Card::new(Rank::Ace, Suit::Clubs), BYTES_AC);
+        cards.insert(Card::new(Rank::Deuce, Suit::Clubs), BYTES_2_OF_CLUBS);
+        cards.insert(Card::new(Rank::Trey, Suit::Clubs), BYTES_3_OF_CLUBS);
+        cards.insert(Card::new(Rank::Four, Suit::Clubs), BYTES_4_OF_CLUBS);
+        cards.insert(Card::new(Rank::Five, Suit::Clubs), BYTES_5_OF_CLUBS);
+        cards.insert(Card::new(Rank::Six, Suit::Clubs), BYTES_6_OF_CLUBS);
+        cards.insert(Card::new(Rank::Seven, Suit::Clubs), BYTES_7_OF_CLUBS);
+        cards.insert(Card::new(Rank::Eight, Suit::Clubs), BYTES_8_OF_CLUBS);
+        cards.insert(Card::new(Rank::Nine, Suit::Clubs), BYTES_9_OF_CLUBS);
+        cards.insert(Card::new(Rank::Ten, Suit::Clubs), BYTES_10_OF_CLUBS);
+        cards.insert(Card::new(Rank::Jack, Suit::Clubs), BYTES_JACK_OF_CLUBS);
+        cards.insert(Card::new(Rank::Queen, Suit::Clubs), BYTES_QUEEN_OF_CLUBS);
+        cards.insert(Card::new(Rank::King, Suit::Clubs), BYTES_KING_OF_CLUBS);
+        cards.insert(Card::new(Rank::Ace, Suit::Clubs), BYTES_ACE_OF_CLUBS);
 
-    cards.insert(Card::new(Rank::Deuce, Suit::Diamonds), BYTES_2D);
-    cards.insert(Card::new(Rank::Trey, Suit::Diamonds), BYTES_3D);
-    cards.insert(Card::new(Rank::Four, Suit::Diamonds), BYTES_4D);
-    cards.insert(Card::new(Rank::Five, Suit::Diamonds), BYTES_5D);
-    cards.insert(Card::new(Rank::Six, Suit::Diamonds), BYTES_6D);
-    cards.insert(Card::new(Rank::Seven, Suit::Diamonds), BYTES_7D);
-    cards.insert(Card::new(Rank::Eight, Suit::Diamonds), BYTES_8D);
-    cards.insert(Card::new(Rank::Nine, Suit::Diamonds), BYTES_9D);
-    cards.insert(Card::new(Rank::Ten, Suit::Diamonds), BYTES_TD);
-    cards.insert(Card::new(Rank::Jack, Suit::Diamonds), BYTES_JD);
-    cards.insert(Card::new(Rank::Queen, Suit::Diamonds), BYTES_QD);
-    cards.insert(Card::new(Rank::King, Suit::Diamonds), BYTES_KD);
-    cards.insert(Card::new(Rank::Ace, Suit::Diamonds), BYTES_AD);
+        cards.insert(Card::new(Rank::Deuce, Suit::Diamonds), BYTES_2_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Trey, Suit::Diamonds), BYTES_3_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Four, Suit::Diamonds), BYTES_4_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Five, Suit::Diamonds), BYTES_5_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Six, Suit::Diamonds), BYTES_6_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Seven, Suit::Diamonds), BYTES_7_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Eight, Suit::Diamonds), BYTES_8_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Nine, Suit::Diamonds), BYTES_9_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Ten, Suit::Diamonds), BYTES_10_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Jack, Suit::Diamonds), BYTES_JACK_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Queen, Suit::Diamonds), BYTES_QUEEN_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::King, Suit::Diamonds), BYTES_KING_OF_DIAMONDS);
+        cards.insert(Card::new(Rank::Ace, Suit::Diamonds), BYTES_ACE_OF_DIAMONDS);
 
-    cards.insert(Card::new(Rank::Deuce, Suit::Hearts), BYTES_2H);
-    cards.insert(Card::new(Rank::Trey, Suit::Hearts), BYTES_3H);
-    cards.insert(Card::new(Rank::Four, Suit::Hearts), BYTES_4H);
-    cards.insert(Card::new(Rank::Five, Suit::Hearts), BYTES_5H);
-    cards.insert(Card::new(Rank::Six, Suit::Hearts), BYTES_6H);
-    cards.insert(Card::new(Rank::Seven, Suit::Hearts), BYTES_7H);
-    cards.insert(Card::new(Rank::Eight, Suit::Hearts), BYTES_8H);
-    cards.insert(Card::new(Rank::Nine, Suit::Hearts), BYTES_9H);
-    cards.insert(Card::new(Rank::Ten, Suit::Hearts), BYTES_TH);
-    cards.insert(Card::new(Rank::Jack, Suit::Hearts), BYTES_JH);
-    cards.insert(Card::new(Rank::Queen, Suit::Hearts), BYTES_QH);
-    cards.insert(Card::new(Rank::King, Suit::Hearts), BYTES_KH);
-    cards.insert(Card::new(Rank::Ace, Suit::Hearts), BYTES_AH);
+        cards.insert(Card::new(Rank::Deuce, Suit::Hearts), BYTES_2_OF_HEARTS);
+        cards.insert(Card::new(Rank::Trey, Suit::Hearts), BYTES_3_OF_HEARTS);
+        cards.insert(Card::new(Rank::Four, Suit::Hearts), BYTES_4_OF_HEARTS);
+        cards.insert(Card::new(Rank::Five, Suit::Hearts), BYTES_5_OF_HEARTS);
+        cards.insert(Card::new(Rank::Six, Suit::Hearts), BYTES_6_OF_HEARTS);
+        cards.insert(Card::new(Rank::Seven, Suit::Hearts), BYTES_7_OF_HEARTS);
+        cards.insert(Card::new(Rank::Eight, Suit::Hearts), BYTES_8_OF_HEARTS);
+        cards.insert(Card::new(Rank::Nine, Suit::Hearts), BYTES_9_OF_HEARTS);
+        cards.insert(Card::new(Rank::Ten, Suit::Hearts), BYTES_10_OF_HEARTS);
+        cards.insert(Card::new(Rank::Jack, Suit::Hearts), BYTES_JACK_OF_HEARTS);
+        cards.insert(Card::new(Rank::Queen, Suit::Hearts), BYTES_QUEEN_OF_HEARTS);
+        cards.insert(Card::new(Rank::King, Suit::Hearts), BYTES_KING_OF_HEARTS);
+        cards.insert(Card::new(Rank::Ace, Suit::Hearts), BYTES_ACE_OF_HEARTS);
 
-    cards.insert(Card::new(Rank::Deuce, Suit::Spades), BYTES_2S);
-    cards.insert(Card::new(Rank::Trey, Suit::Spades), BYTES_3S);
-    cards.insert(Card::new(Rank::Four, Suit::Spades), BYTES_4S);
-    cards.insert(Card::new(Rank::Five, Suit::Spades), BYTES_5S);
-    cards.insert(Card::new(Rank::Six, Suit::Spades), BYTES_6S);
-    cards.insert(Card::new(Rank::Seven, Suit::Spades), BYTES_7S);
-    cards.insert(Card::new(Rank::Eight, Suit::Spades), BYTES_8S);
-    cards.insert(Card::new(Rank::Nine, Suit::Spades), BYTES_9S);
-    cards.insert(Card::new(Rank::Ten, Suit::Spades), BYTES_TS);
-    cards.insert(Card::new(Rank::Jack, Suit::Spades), BYTES_JS);
-    cards.insert(Card::new(Rank::Queen, Suit::Spades), BYTES_QS);
-    cards.insert(Card::new(Rank::King, Suit::Spades), BYTES_KS);
-    cards.insert(Card::new(Rank::Ace, Suit::Spades), BYTES_AS);
-
-    cards
+        cards.insert(Card::new(Rank::Deuce, Suit::Spades), BYTES_2_OF_SPADES);
+        cards.insert(Card::new(Rank::Trey, Suit::Spades), BYTES_3_OF_SPADES);
+        cards.insert(Card::new(Rank::Four, Suit::Spades), BYTES_4_OF_SPADES);
+        cards.insert(Card::new(Rank::Five, Suit::Spades), BYTES_5_OF_SPADES);
+        cards.insert(Card::new(Rank::Six, Suit::Spades), BYTES_6_OF_SPADES);
+        cards.insert(Card::new(Rank::Seven, Suit::Spades), BYTES_7_OF_SPADES);
+        cards.insert(Card::new(Rank::Eight, Suit::Spades), BYTES_8_OF_SPADES);
+        cards.insert(Card::new(Rank::Nine, Suit::Spades), BYTES_9_OF_SPADES);
+        cards.insert(Card::new(Rank::Ten, Suit::Spades), BYTES_10_OF_SPADES);
+        cards.insert(Card::new(Rank::Jack, Suit::Spades), BYTES_JACK_OF_SPADES);
+        cards.insert(Card::new(Rank::Queen, Suit::Spades), BYTES_QUEEN_OF_SPADES);
+        cards.insert(Card::new(Rank::King, Suit::Spades), BYTES_KING_OF_SPADES);
+        cards.insert(Card::new(Rank::Ace, Suit::Spades), BYTES_ACE_OF_SPADES);
+        cards
+    });
 });
 
 /// A collection of cards textures used for drawing.
@@ -155,7 +155,7 @@ impl Textures {
             })
             .collect();
 
-        let back = ctx.load_texture("back", image_from_memory(BYTES_BB), Default::default());
+        let back = ctx.load_texture("back", image_from_memory(BYTES_CARD_BACK), Default::default());
 
         Self { cards, back }
     }
