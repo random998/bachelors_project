b97cd70d0723936f1177a46e9e8e91f459c38608
marvin <marvin.stoetzel@mailbox.org>
2025-07-03 15:20:20 +0200

    remove engine.rs and start migrating code from table-engine crate to core crate

--- crates/table-engine/src/lib.rs
+++ crates/table-engine/src/lib.rs
@@ -1,7 +1,6 @@
 //! Pure game-logic for a Texas-Hold'em table.  No networking, no storage.
 
 pub mod engine;
-pub mod player;
 pub mod players_state;
 
 pub use engine::{EngineCallbacks, InternalTableState};
