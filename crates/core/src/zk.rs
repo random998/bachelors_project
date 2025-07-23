d90d71603af008814993ff6d1df12b2e00a880bd
marvin <marvin.stoetzel@mailbox.org>
2025-07-23 15:33:25 +0200

    all test run correctly as expected

--- crates/core/src/zk.rs
+++ crates/core/src/zk.rs
@@ -1,8 +1,8 @@
 // poker_core/src/zk.rs
 use serde::{Deserialize, Serialize};
-#[derive(Clone, Debug, Serialize, Deserialize, Default,)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, PartialEq)]
 pub struct Proof(Vec<u8,>,);
-#[derive(Clone, Debug, Serialize, Deserialize, Default,)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, PartialEq)]
 pub struct Commitment(Vec<u8,>,);
 pub type ShuffleProof = Proof;
 pub type RangeProof = Proof;
